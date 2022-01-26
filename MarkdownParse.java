55c0abb952f73c73646e9d5cb26234d6f39d91a6
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2022-01-26 15:47:53 -0800

    new testfiles and update markdownparse

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,12 +15,17 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(markdown.charAt(nextOpenBracket)=='!'){
+            if(nextOpenBracket != 0 && markdown.charAt(nextOpenBracket-1)=='!'){
                 currentIndex = closeParen+1;
                 continue;
             }
+            if(openParen == -1){
+                return toReturn;
+            }
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            
         }
         return toReturn;
     }
---
+++ firstline.md
@@ -0,0 +1,1 @@
+[Link](https://help.com)
