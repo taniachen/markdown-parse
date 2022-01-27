51daf2ff9e67e9660b8045a362b44a4a1efd7b47
Irene Liao <iireneliao@gmail.com>
2022-01-26 16:09:35 -0800

    TestCase1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,11 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -8,8 +15,13 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() {
-        assertEquals("[https://something.com, some-page.html]
-        test see", actual);
+    public void testGetLinks() throws IOException {
+        ArrayList<String> linkList = new ArrayList<String>();
+        Path file1 = Path.of("test-file.md");
+        String contents = Files.readString(file1);
+        linkList.add("https://something.com");
+        linkList.add("some-page.html");
+
+        assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(contents));
     }
 }
