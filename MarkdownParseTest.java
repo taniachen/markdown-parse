f0005eb4e1233fd36f5521a93223928da84629d2
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2022-02-02 15:21:59 -0800

    Update MarkdownParseTest.java
    add command comments

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.*;
 
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
